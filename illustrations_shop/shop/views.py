16fad2d091fd2ad00ae4117cc476d01914c13ac7
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-06-09 01:38:56 +0100

    Add view for about page

--- illustrations_shop/shop/views.py
+++ illustrations_shop/shop/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render, redirect
 def home(request):   
     return render(request, 'shop/home.html')
 
+def about(request):
+    return render(request, 'shop/about.html')
+
+
 
