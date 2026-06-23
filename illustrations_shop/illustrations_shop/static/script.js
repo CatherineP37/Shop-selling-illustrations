2e83ecdbc01bd76e9a53a1f7bd834d293f7afe3a
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-06-23 05:07:46 +0100

    Add JavaScript function for opening the mobile menu

--- illustrations_shop/illustrations_shop/static/script.js
+++ illustrations_shop/illustrations_shop/static/script.js
@@ -1,4 +1,16 @@
+const mobileMenu = document.getElementById('mobileMenu');
+const opened = document.getElementById('opened');
+const closed = document.getElementById('closed');
 const filter = document.querySelector('.filter');
+
+function openMobileMenu() {    
+    mobileMenu.style.display = "flex";
+    mobileMenu.style.flexDirection = "column";
+    mobileMenu.style.justifyContent = "center";
+    mobileMenu.style.alignItems = "center";    
+    closed.style.display = "none";
+    opened.style.display = "block";
+}
 
 function toggleFilter() {
     filter.classList.toggle('filter-open');
